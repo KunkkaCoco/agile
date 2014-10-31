1bf7e57fdff9dfce0b228e82409c410afed32745
chenweichao <chenweichao@news.cn>
2014-10-31 16:03:45 +0800

    init

---
+++ src/test/java/com/test/JunitTest.java
@@ -0,0 +1,43 @@
+package com.test;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import thinkInjava.c04.Flower;
+
+public class JunitTest {
+	private static Logger logger = LoggerFactory.getLogger(JunitTest.class);
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println("before test##########################################");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		System.out.println("AfterClass test##########################################");
+	}
+
+	// @Test
+	public void testSublist() {
+
+		logger.debug("-----test-----");
+	}
+
+	@Test
+	public void testFlower() {
+
+		Flower x = new Flower();
+		x.print();
+	}
+
+	@Test
+	public void testconsole() {
+
+		System.console();
+	}
+
+}
